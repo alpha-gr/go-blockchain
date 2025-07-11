8b607b37ea7104fb9b98ade4ddfcfc60d107b49d
alpha-gr <grande.alfry@gmail.com>
2025-07-11 22:52:23 +0200

    added interactive cli

--- cli/cli.go
+++ cli/cli.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"go-blockchain/blockchain"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"runtime"
 	"strconv"
+	"strings"
 )
 
 type CommandLine struct {
@@ -18,7 +20,9 @@ func (cli *CommandLine) printUsage() {
 	fmt.Println(" getbalance -address ADDRESS - Get balance of an address")
 	fmt.Println(" createblockchain -address ADDRESS - Create a new blockchain and the genesis block")
 	fmt.Println(" send -from FROM -to TO -amount AMOUNT - Send amount from one address to another")
+	fmt.Println(" mine -address ADDRESS - Mine a new block and receive mining reward")
 	fmt.Println(" printchain - Print the blocks in the blockchain")
+	fmt.Println(" interactive - Start interactive mode for continuous command execution")
 }
 
 func (cli *CommandLine) validateArgs() {
@@ -79,6 +83,97 @@ func (cli *CommandLine) send(from, to string, amount int) {
 	fmt.Printf("Transaction sent from %s to %s for amount %d successfully\n", from, to, amount)
 }
 
+func (cli *CommandLine) mine(address string) {
+	chain := blockchain.ContinueBlockChain(address)
+	defer chain.Database.Close()
+
+	tx := blockchain.CoinbaseTx(address, "Mining reward")
+	chain.AddBlock([]*blockchain.Transaction{tx})
+	fmt.Printf("Block mined successfully! Mining reward of 100 coins sent to %s\n", address)
+}
+
+func (cli *CommandLine) interactiveMode() {
+	fmt.Println("=== Blockchain Interactive Mode ===")
+	fmt.Println("Type 'help' for available commands, 'exit' or 'quit' to leave")
+	fmt.Println()
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("blockchain> ")
+		if !scanner.Scan() {
+			break
+		}
+
+		input := strings.TrimSpace(scanner.Text())
+		if input == "" {
+			continue
+		}
+
+		// Split input into arguments
+		args := strings.Fields(input)
+		command := args[0]
+
+		switch command {
+		case "exit", "quit":
+			fmt.Println("Exiting interactive mode...")
+			return
+		case "help":
+			cli.printInteractiveHelp()
+		case "getbalance":
+			if len(args) != 2 {
+				fmt.Println("Usage: getbalance <address>")
+				continue
+			}
+			cli.getBalance(args[1])
+		case "createblockchain":
+			if len(args) != 2 {
+				fmt.Println("Usage: createblockchain <address>")
+				continue
+			}
+			cli.createBlockChain(args[1])
+		case "printchain":
+			cli.printChain()
+		case "send":
+			if len(args) != 4 {
+				fmt.Println("Usage: send <from> <to> <amount>")
+				continue
+			}
+			amount, err := strconv.Atoi(args[3])
+			if err != nil {
+				fmt.Println("Error: amount must be a valid number")
+				continue
+			}
+			if amount <= 0 {
+				fmt.Println("Error: amount must be greater than 0")
+				continue
+			}
+			cli.send(args[1], args[2], amount)
+		case "mine":
+			if len(args) != 2 {
+				fmt.Println("Usage: mine <address>")
+				continue
+			}
+			cli.mine(args[1])
+		default:
+			fmt.Printf("Unknown command: %s\n", command)
+			fmt.Println("Type 'help' for available commands")
+		}
+		fmt.Println()
+	}
+}
+
+func (cli *CommandLine) printInteractiveHelp() {
+	fmt.Println("Available commands:")
+	fmt.Println("  getbalance <address>          - Get balance of an address")
+	fmt.Println("  createblockchain <address>    - Create a new blockchain and genesis block")
+	fmt.Println("  send <from> <to> <amount>     - Send amount from one address to another")
+	fmt.Println("  mine <address>                - Mine a new block and receive mining reward")
+	fmt.Println("  printchain                    - Print all blocks in the blockchain")
+	fmt.Println("  help                          - Show this help message")
+	fmt.Println("  exit, quit                    - Exit interactive mode")
+}
+
 func (cli *CommandLine) Run() {
 	cli.validateArgs()
 
@@ -86,12 +181,14 @@ func (cli *CommandLine) Run() {
 	createBlockchainCmd := flag.NewFlagSet("createblockchain", flag.ExitOnError)
 	sendCmd := flag.NewFlagSet("send", flag.ExitOnError)
 	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+	mineCmd := flag.NewFlagSet("mine", flag.ExitOnError)
 
 	getBalanceAddress := getBalanceCmd.String("address", "", "Address to get balance for")
 	createBlockchainAddress := createBlockchainCmd.String("address", "", "Address to create blockchain for")
 	sendFrom := sendCmd.String("from", "", "Source wallet address")
 	sendTo := sendCmd.String("to", "", "Destination wallet address")
 	sendAmount := sendCmd.Int("amount", 0, "Amount to send")
+	mineAddress := mineCmd.String("address", "", "Address to receive mining reward")
 
 	switch os.Args[1] {
 	case "getbalance":
@@ -112,6 +209,14 @@ func (cli *CommandLine) Run() {
 		if err != nil {
 			log.Panic(err)
 		}
+	case "mine":
+		err := mineCmd.Parse(os.Args[2:])
+		if err != nil {
+			log.Panic(err)
+		}
+	case "interactive":
+		cli.interactiveMode()
+		return
 	default:
 		cli.printUsage()
 		runtime.Goexit()
@@ -143,6 +248,14 @@ func (cli *CommandLine) Run() {
 			runtime.Goexit()
 		}
 		cli.send(*sendFrom, *sendTo, *sendAmount)
+	}
+
+	if mineCmd.Parsed() {
+		if *mineAddress == "" {
+			mineCmd.Usage()
+			runtime.Goexit()
+		}
+		cli.mine(*mineAddress)
 	}
 
 }
